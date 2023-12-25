5d997c42231129c017c552005b9d01846679edf0
usuario360 <154806227+usuario360@users.noreply.github.com>
2023-12-25 12:48:13 +0100

    Update viral_videos.py

--- viral_videos.py
+++ viral_videos.py
@@ -1,78 +1,46 @@
 import os
-import requests
 from pytube import YouTube
-from moviepy.editor import VideoFileClip, TextClip, concatenate_videoclips, CompositeVideoClip
+from moviepy.editor import VideoFileClip, concatenate_videoclips
 
-def find_viral_videos(api_key):
-    # (unchanged)
-
-def download_clip(video_url, output_path):
-    # (unchanged)
-
-def clip_video(video_path, output_path):
-    # (unchanged)
-
-def clip_videos(viral_videos, output_path):
-    # (unchanged)
-
-def load_video_clip(video_path):
+def download_video(video_url, output_path):
     try:
-        return VideoFileClip(video_path)
+        yt = YouTube(video_url)
+        stream = yt.streams.filter(file_extension="mp4", res="360p").first()
+        stream.download(output_path)
+        print(f"Downloaded: {yt.title}")
+        return os.path.join(output_path, f"{yt.title}.mp4")
     except Exception as e:
-        print(f"Error loading video: {e}")
+        print(f"Error downloading video: {e}")
         return None
 
-def process_video(video_path):
-    clip = load_video_clip(video_path)
-    if clip:
-        duration = clip.duration
-        print(f"Video: {video_path}, Duration: {duration} seconds")
-        clip.close()
-    else:
-        print(f"Skipping {video_path} due to error loading video.")
-
-def process_videos_in_folder(folder_path):
-    video_files = [f for f in os.listdir(folder_path) if f.endswith(".mp4")]
-    for video_file in video_files:
-        video_path = os.path.join(folder_path, video_file)
-        process_video(video_path)
-
-def make_tiktok_video(viral_videos, output_path):
-    clips = []
-    for video in viral_videos:
-        video_id = video['id']
-        clip_path = os.path.join(output_path, f"{video_id}_clip.mp4")
-
-        if os.path.exists(clip_path):
-            clips.append(VideoFileClip(clip_path))
-
+def clip_video(input_path, output_path, duration=5):
     try:
-        concatenated_clip = concatenate_videoclips(clips, method="compose")
-
-        text = TextClip("This is a TikTok video made by an AI!", fontsize=24, color='white', bg_color='black')
-        text = text.set_pos('center')
-        text = text.set_duration(concatenated_clip.duration)
-        final_video = CompositeVideoClip([concatenated_clip, text])
-
-        final_video_path = os.path.join(output_path, "tiktok_video.mp4")
-        final_video.write_videofile(final_video_path)
-
-        concatenated_clip.close()
-        final_video.close()
-
-        print(f"Processed TikTok Video Path: {final_video_path}")
-
+        clip = VideoFileClip(input_path)
+        clipped = clip.subclip(0, duration)
+        clipped.write_videofile(output_path, codec="libx264", audio_codec="aac")
+        print(f"Clipped and saved: {output_path}")
+        clip.close()
+        clipped.close()
     except Exception as e:
-        print(f"Error making TikTok video: {e}")
+        print(f"Error clipping video: {e}")
 
 def main():
-    api_key = "AIzaSyAJ2e9oVr-_7F-bOlZtVVx5BZxuRh1yY6c"  # Replace with your actual YouTube API key
-    output_path = "viral_videos"
+    video_url = "https://youtu.be/QdBZY2fkU-0?si=k-AhLFNf6imtERlf"  # Replace with the actual YouTube video URL
+    output_folder = "downloaded_videos"
+    output_clips_folder = "clipped_videos"
 
-    viral_videos = find_viral_videos(api_key)
-    clip_videos(viral_videos, output_path)
-    process_videos_in_folder(output_path)
-    make_tiktok_video(viral_videos, output_path)
+    # Create folders if they don't exist
+    os.makedirs(output_folder, exist_ok=True)
+    os.makedirs(output_clips_folder, exist_ok=True)
+
+    # Download video
+    downloaded_video_path = download_video(video_url, output_folder)
+
+    if downloaded_video_path:
+        # Clip video to 5 seconds
+        output_clip_path = os.path.join(output_clips_folder, "clipped_video.mp4")
+        clip_video(downloaded_video_path, output_clip_path, duration=5)
 
 if __name__ == "__main__":
     main()
+
